aee51e46d1b1af0a6cf662bd6a4721b1e4e377c9
ys <go.xlv@qq.com>
2017-08-08 14:13:40 +0800

    plugin注释

--- app/plugins/Sample.php
+++ app/plugins/Sample.php
@@ -7,21 +7,27 @@
  */
 class SamplePlugin extends Yaf_Plugin_Abstract {
 
+    // 在路由之前触发
 	public function routerStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
 
+    // 路由结束之后触发
 	public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
 
+	// 分发循环开始之前触发
 	public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
-
+	
+	// 分发之前触发
 	public function preDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
 
+	// 分发结束之后触发
 	public function postDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
 
+	// 分发循环结束之后触发
 	public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
 	}
 }
