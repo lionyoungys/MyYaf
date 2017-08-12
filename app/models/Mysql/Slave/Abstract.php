86d91c548b871e36ab6a8254d83560ee5a47e221
ys <go.xlv@qq.com>
2017-08-12 18:48:08 +0800

    新增从数据库配置

--- app/models/Mysql/Slave/Abstract.php
+++ app/models/Mysql/Slave/Abstract.php
@@ -1,15 +1,11 @@
 <?php
-namespace Mysql;
+namespace Mysql\Slave;
 /**
- * Mysql主数据访问抽象模型
+ * Mysql从数据访问抽象模型
  * @package Mysql
  * @author 帅
  */
-abstract class AbstractModel {
-
-    //禁止克隆类
-    private function __clone(){}        
-
+abstract class AbstractModel extends \Mysql\AbstractModel {
     /**
      * 获取数据库连接对象
      *
@@ -20,25 +16,13 @@ abstract class AbstractModel {
         static $_instance = null;
         //判断对象是否已创建
         if (!$_instance) {
-            $Config = \Yaf_Registry::get('config')->get('Mysql');    //获取数据库配置
+            $Config = \Yaf_Registry::get('config')->get('Mysql.Slave');    //获取数据库配置
             //创建数据库对象
             $_instance = new \Medoo\Medoo($Config->toArray());
         }
         return $_instance;
     }
 
-    /**
-     * 去除类名中的命名空间
-     *
-     * @param string $name
-     * @return string
-     */
-    protected static function trimNamespace($name)
-    {
-        $dataArr = explode('\\',$name);
-        $realName = array_pop($dataArr);
-        return $realName;
-    }
 
     /**
      * 映射模型静态方法
@@ -54,6 +38,5 @@ abstract class AbstractModel {
         array_unshift($args, $Model);                            //将数据表名追加至参数列表数组
         return call_user_func_array(array(self::_getAdapter(),$name),$args);    //调用DB数据库对象的$name方法,参数为$args数组列表值
     }
-
 
 }
